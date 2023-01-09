f87eeea2b8c9dafe47c09530d5e29e857cffcf60
tinghui2012 <tinghui2012@yahoo.com>
2023-01-09 11:59:42 -0500

    Added intro

---
+++ components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Hello!</h1>
+        <p>
+            I'm Christine. I'm an undergrad at Columbia studying computer science and linguistics. I am from San Francisco Bay Area and Shanghai. I love traveling, reading, dancing, and cooking.
+        </p>
+        <a href="https://www.instagram.com/christinelitinghui" target="_blank">Instagram</a>
+        <a href="https://www.github.com/tinghui2012" target="_blank">Github</a>
+    </div>
+</template>
